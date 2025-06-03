cde7a78bbbf3f6ec12675ce990b3c7a758c73269
YugandharRepo <yugandhar.g414@gmail.com>
2025-06-03 11:13:23 +0530

    Initial commit12

--- index.js
+++ index.js
@@ -1,34 +1,118 @@
-const express = require("express");
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
 
+// const express = require('express');
+// const http = require('http');
+// const cors = require('cors');
+// const { Server } = require('socket.io');
+
+// const app = express();
+// app.use(cors());
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: { origin: '*' },
+// });
+
+// let users = {};
+
+// io.on('connection', socket => {
+//   console.log('User connected:', socket.id);
+
+//   socket.on('locationUpdate', data => {
+//     console.log('Location from', data.userId, ':', data.latitude, data.longitude);
+//     users[data.userId] = data;
+//     socket.broadcast.emit('locationBroadcast', data); // Send to others
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('User disconnected:', socket.id);
+//   });
+// });
+
+// server.listen(3000, () => console.log('Socket server running on port 3000'));
+
+
+
+
+
+
+
+
+// const express = require('express');
+
+// const http = require('http');
+
+// const socketIO = require('socket.io');
+
+// const cors = require('cors');
+ 
+// const app = express();
+
+// const server = http.createServer(app);
+
+// const io = socketIO(server, {
+
+//   cors: {
+
+//     origin: '*', // allow all for dev
+
+//     methods: ['GET', 'POST']
+
+//   }
+
+// });
+ 
+// app.use(cors());
+ 
+// io.on('connection', (socket) => {
+
+//   console.log('User connected:', socket.id);
+ 
+//   socket.on('send-location', (location) => {
+
+//     // Broadcast location to other clients
+
+//     socket.broadcast.emit('receive-location', location);
+
+//   });
+ 
+//   socket.on('disconnect', () => {
+
+//     console.log('User disconnected:', socket.id);
+
+//   });
+
+// });
+ 
+// const PORT = 3000;
+
+// server.listen(PORT, () => console.log(`Socket.IO server running on port ${PORT}`));
+
+ // server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 const app = express();
-app.use(cors());
-
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
-    origin: "http://43.205.43.29:3000", // your frontend URL
-    methods: ["GET", "POST"],
+    origin: '*', // Set this carefully in production
   },
 });
 
-io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
 
-  socket.on("send_message", (data) => {
-    io.emit("receive_message", data);
+  socket.on('locationUpdate', (data) => {
+    console.log('Received location:', data);
+    // Broadcast to others
+    socket.broadcast.emit('locationBroadcast', data);
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected", socket.id);
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
   });
 });
 
-
-
-server.listen(3001, () => {
-  console.log("Server running on 3001");
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
